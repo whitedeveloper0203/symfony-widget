a19ee07e10709f4be748dfbcb63742360f652eca
fairy <whitedeveloper0203@mail.ru>
2019-09-17 00:41:05 +1000

    fix issues

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\WidgetPlacement;
 
 class DefaultController extends Controller
@@ -14,21 +15,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $widget = new WidgetPlacement();
-        $widget->setType('facebook');
-        $widget->setDensity(19.9);
-        $widget->setEnabled(true);
-        $widget->setPlacement('Ergonomic and stylish!');
-        $widget->setUrl('123');
-
-        $entityManager = $this->getDoctrine()->getManager();
-
-        // tells Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($widget);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
@@ -37,7 +23,7 @@ class DefaultController extends Controller
 
 
     /**
-     * @Route("/widgetplacement", name="homepage_widgetplacement")
+     * @Route("/widgetplacement", name="api_widgets_placements")
      */
     public function widgetplacementAction(Request $request)
     {
@@ -47,6 +33,7 @@ class DefaultController extends Controller
         $maxItems   = $request->get('max_items');
         $placement  = $request->get('placement');
         $pageSize   = $request->get('page_size');
+        $totalPage  = $request->get('total_page');
 
         $enabledWidgets = $widgetRepo->createQueryBuilder('p')
                             ->andWhere('p.enabled = :enabled')
@@ -57,51 +44,68 @@ class DefaultController extends Controller
         
         $results = [];
         $densities = [];
-        // Get maxItem*10 widgets by density
+        // Get maxItem*pageSize widgets by density
         $sumDensity = 0;
         foreach($enabledWidgets as $widget)
         {
-            $tDensity = (object) [  'id' => $widget->getId(), 
-                                    'to_density' => $sumDensity, 
-                                    'from_density' => $sumDensity+$widget->getDensity(),
-                                    'selected' => false
+            $tDensity = (object) [  'id'            => $widget->getId(), 
+                                    'to_density'    => $sumDensity, 
+                                    'from_density'  => $sumDensity+$widget->getDensity(),
+                                    'selected'      => false
                                 ];
             $sumDensity += $widget->getDensity();
             $densities[] = $tDensity;
         } 
 
-        $selectedCount = 0;
-        while( 1 ) {
-            
-            if($selectedCount == count($densities) || $selectedCount >= $maxItems * 10)
-                break;
-            
-            // Generate random value between 0, $sumDensity
-            $random = rand(0, $sumDensity*10)/10;
-            
-            foreach($densities as $density) {
-                // Check if the generated value in the range of density
-                if(($density->to_density <= $random) && ($random < $density->from_density) && ($density->selected == false)) {
-                    $selectedCount ++;
-                    $density->selected = true;
+        $startPlaceIndex = $maxItems * $pageSize * ($totalPage - 1);
+        // Generate 4 pages
+        for( $index = 0; $index < 4; $index ++)
+        {
+            $selectedCount = 0;
+            // Generate random places
+            $places = range($index*$pageSize + $startPlaceIndex, ($index+1)*$pageSize-1 + $startPlaceIndex);
+            shuffle($places);
 
-                    // Add widget to result
-                    $widget = $widgetRepo->find($density->id);
-                    $tResult = (object) [   'type' => $widget->getType(), 
-                                            'place' => $selectedCount,
-                                            'url' => $widget->getUrl()
-                                        ];
-                    $result[] = $tResult;
+            while( $selectedCount < $maxItems ) {
+                
+                // Generate random value between 0, $sumDensity
+                $random = rand(0, $sumDensity*10)/10;
+                
+                foreach($densities as $density) {
+                    // Check if the generated value in the range of density
+                    if(($density->to_density <= $random) && ($random < $density->from_density)) {
+                        $selectedCount ++;
+                        $density->selected = true;
+
+                        // Add widget to result
+                        $widget = $widgetRepo->find($density->id);
+                        $tResult = (object) [   'type'  => $widget->getType(), 
+                                                'place' => $places[$selectedCount],
+                                                'url'   => $widget->getUrl()
+                                            ];
+                        $results[] = $tResult;
+                    }
                 }
             }
         }
+        
+        // Sort by place number
+        usort($results, function($a, $b){
+            return $a->place > $b->place;
+        });
 
         $response = new Response();
-        $response->setContent(json_encode($result));
+        $response->setContent(json_encode($results));
 
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
 
         return $response;
     }
+
+    function cmp($a, $b)
+    {
+        return ($a->place) > ($b->place);
+    }
+
 }
